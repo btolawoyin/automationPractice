193fa381f48c0584d756bba55a97867621eb9d72
Bukky Olawoyin <bukky@olawoyin.net>
2022-01-29 13:40:45 +0000

    Removed automationPractice.iml, Added ScrapBoard.java and jarRepositories.xml

---
+++ src/test/java/Playground/ScrapBoard.java
@@ -0,0 +1,23 @@
+package Playground;
+
+
+import org.junit.Test;
+
+public class ScrapBoard {
+
+    @Test
+    public void setDay() {
+        int day = 4;
+
+        switch (day) {
+            case 1:
+                System.out.println("Saturday");
+                break;
+            case 2:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Weekend");
+        }
+    }
+}
